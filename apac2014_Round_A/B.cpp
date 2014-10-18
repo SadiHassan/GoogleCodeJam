3d6ad3f1b1b571ab062b10d07fdeb5eb3316bd31
sadi.research@gmail.com <sadi.research@gmail.com>
2014-10-19 00:34:00 +0600

    Solved Problem B

--- apac2014_Round_A/B.cpp
+++ apac2014_Round_A/B.cpp
@@ -1,30 +1,169 @@
 #include<iostream>
 #include<stdio.h>
+#include<string.h>
 using namespace std;
 
 int T, N;
 int board[22][22];
+bool merged[22][22];
+
+void print_board(){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++)  cout<<board[i][j]<<" ";
+        cout<<endl;
+    }
+}
 
 void left_move(){
 
+    int k, tmp;
+    memset(merged,false,sizeof(merged));
+
+    for(int i=0; i<N; i++){
+
+        for(int j=0; j<N; j++){
+
+            if(board[i][j]!=0){
+                k = j;
+                while(k>0){
+                    if(board[i][k-1]==0){
+                        board[i][k-1] = board[i][k];
+                        board[i][k] = 0;
+                        if(merged[i][k]==true) merged[i][k-1]=true;
+                    }
+                    else if(merged[i][k-1]==true) break;
+                    else if (board[i][k]!=board[i][k-1]) break;
+                    else if (!merged[i][k]&&!merged[i][k-1]){
+                            board[i][k-1] = board[i][k-1]*2;
+                            board[i][k] = 0;
+                            merged[i][k-1] = true;
+
+                    }
+
+                    k--;
+                }
+            }
+        }
+
+}
+
+print_board();
 }
 
 void right_move(){
+
+    int k, tmp;
+    memset(merged,false,sizeof(merged));
+
+    for(int i=0; i<N; i++){
+
+        for(int j=N-1; j>=0; j--){
+
+            if(board[i][j]!=0){
+                k = j;
+
+                while(k<N-1){
+                    if(board[i][k+1]==0){
+                        board[i][k+1] = board[i][k];
+                        board[i][k] = 0;
+                        if(merged[i][k]==true) merged[i][k+1]=true;
+                    }
+                    else if(merged[i][k+1]==true) break;
+                    else if (board[i][k]!=board[i][k+1]) break;
+                    else if (!merged[i][k]&&!merged[i][k+1]){
+                            board[i][k+1] = board[i][k+1]*2;
+                            board[i][k] = 0;
+                            merged[i][k+1] = true;
+                    }
+
+                    k++;
+                }
+            }
+        }
+
+}
+
+print_board();
 
 }
 
 void up_move(){
 
+int k, tmp;
+    memset(merged,false,sizeof(merged));
+
+    for(int j=0; j<N; j++){
+
+        for(int i=0; i<N; i++){
+
+            if(board[i][j]!=0){
+                k = i;
+                while(k>0){
+                    if(board[k-1][j]==0){
+                        board[k-1][j] = board[k][j];
+                        board[k][j] = 0;
+                        if(merged[k][j]==true) merged[k-1][j]=true;
+                    }
+                    else if(merged[k-1][j]==true) break;
+                    else if (board[k][j]!=board[k-1][j]) break;
+                    else if (!merged[k][j]&&!merged[k-1][j]){
+                            board[k-1][j] = board[k-1][j]*2;
+                            board[k][j] = 0;
+                            merged[k-1][j] = true;
+
+                    }
+
+                    k--;
+                }
+            }
+        }
+
+}
+print_board();
 }
 
 void down_move(){
 
+int k, tmp;
+
+memset(merged,false,sizeof(merged));
+
+    for(int j=0; j<N; j++){
+
+        for(int i=N-1; i>=0; i--){
+
+            if(board[i][j]!=0){
+                k = i;
+
+                while(k<N-1){
+                    if(board[k+1][j]==0){
+                        board[k+1][j] = board[k][j];
+                        board[k][j] = 0;
+                        if(merged[k][j]==true) merged[k+1][j]=true;
+                    }
+                    else if(merged[k+1][j]==true) break;
+                    else if (board[k][j]!=board[k+1][j]) break;
+                    else if (!merged[k][j]&&!merged[k+1][j]){
+                            board[k+1][j] = board[k+1][j]*2;
+                            board[k][j] = 0;
+                            merged[k+1][j] = true;
+                    }
+
+                    k++;
+                }
+            }
+        }
+
+}
+
+print_board();
 }
 
 int main(){
 
 string dir;
-
+//freopen("B-large-practice.in","r",stdin);
+//freopen("out.txt","w",stdout);
 cin>>T;
 
 for(int t=1; t<=T; t++){
